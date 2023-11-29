e0625f27c4baa85bc911b83824daa32f882651e2
killingjoke2 <corpcorporativos@gmail.com>
2023-11-28 19:22:26 -0500

    Initial commit

---
+++ script-mipagina.js
@@ -0,0 +1,9 @@
+
+//alert("Hola este es mi Javascript");
+//let nombre = "marta";
+//nombre = "maria";
+//var nombre1= "marta";
+//const nombre2 = "marta";
+//console.log (nombre);
+//console.log (nombre1);
+// console.log (nombre2);
